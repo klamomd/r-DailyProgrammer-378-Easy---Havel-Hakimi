ffe47fbddc6ce18742d5a7f9427f59a60006942b
Michael Klamo <michaelklamo@gmail.com>
2020-11-22 09:20:38 -0700

    Updated lazy tests to actual xUnit tests.
    Removed 'main' method due to 'multiple entrypoints' complaint and due to it no longer being necessary

--- DailyProgrammer 378E/DailyProgrammer 378E/Program.cs
+++ DailyProgrammer 378E/DailyProgrammer 378E/Program.cs
@@ -2,24 +2,12 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Xunit;
 
 namespace DailyProgrammer_378E
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
-        {
-            TestRemoveZeroes();
-            TestSortDescending();
-            TestLengthCheck();
-            TestFrontElimination();
-
-            TestHavelHakimi();
-
-            Console.WriteLine("Reached end of Main. Press enter to exit.");
-            Console.ReadLine();
-        }
-
         // Optional Warmup 1: Eliminating 0's
         // Takes in answers in the form of a List<uint>, removes any zeroes, and returns the result.
         private static List<uint> RemoveZeroes(List<uint> answers)
@@ -90,118 +78,179 @@ namespace DailyProgrammer_378E
         // TEST METHODS
 
         // Run provided test examples for Warmup 1 (RemoveZeroes).
-        private static void TestRemoveZeroes()
+        [Fact]
+        public static void TestRemoveZeroes()
         {
+            // Arrange
             List<uint> test1 = new List<uint> { 5, 3, 0, 2, 6, 2, 0, 7, 2, 5 };
-            List<uint> result1 = new List<uint> { 5, 3, 2, 6, 2, 7, 2, 5 };
-            Debug.Assert(RemoveZeroes(test1).SequenceEqual(result1));
+            List<uint> expectedResult1 = new List<uint> { 5, 3, 2, 6, 2, 7, 2, 5 };
 
             List<uint> test2 = new List<uint> { 4, 0, 0, 1, 3 };
-            List<uint> result2 = new List<uint> { 4, 1, 3 };
-            Debug.Assert(RemoveZeroes(test2).SequenceEqual(result2));
+            List<uint> expectedResult2 = new List<uint> { 4, 1, 3 };
 
             List<uint> test3 = new List<uint> { 1, 2, 3 };
-            Debug.Assert(RemoveZeroes(test3).SequenceEqual(test3)); // This one should be the same.
+            List<uint> expectedResult3 = test3;     // This one should be the same.
 
             List<uint> test4 = new List<uint> { 0, 0, 0 };
-            List<uint> result4 = new List<uint> { };
-            Debug.Assert(RemoveZeroes(test4).SequenceEqual(result4));
+            List<uint> expectedResult4 = new List<uint> { };
 
-            List<uint> test5 = new List<uint>();                    // Empty list.
-            Debug.Assert(RemoveZeroes(test5).SequenceEqual(test5)); // This one should be the same.
+            List<uint> test5 = new List<uint>();    // Empty list.
+            List<uint> expectedResult5 = test5;     // This one should be the same.
+
+            // Act
+            List<uint> result1 = RemoveZeroes(test1);
+            List<uint> result2 = RemoveZeroes(test2);
+            List<uint> result3 = RemoveZeroes(test3);
+            List<uint> result4 = RemoveZeroes(test4);
+            List<uint> result5 = RemoveZeroes(test5);
+
+            // Assert
+            Assert.True(result1.SequenceEqual(expectedResult1));
+            Assert.True(result2.SequenceEqual(expectedResult2));
+            Assert.True(result3.SequenceEqual(expectedResult3));
+            Assert.True(result4.SequenceEqual(expectedResult4));
+            Assert.True(result5.SequenceEqual(expectedResult5));
         }
 
         // Run provided test examples for Warmup 2 (SortDescending).
-        private static void TestSortDescending()
+        [Fact]
+        public static void TestSortDescending()
         {
+            // Arrange
             List<uint> test1 = new List<uint> { 5, 1, 3, 4, 2 };
-            List<uint> result1 = new List<uint> { 5, 4, 3, 2, 1 };
-            Debug.Assert(SortDescending(test1).SequenceEqual(result1));
+            List<uint> expectedResult1 = new List<uint> { 5, 4, 3, 2, 1 };
 
             List<uint> test2 = new List<uint> { 0, 0, 0, 4, 0 };
-            List<uint> result2 = new List<uint> { 4, 0, 0, 0, 0 };
-            Debug.Assert(SortDescending(test2).SequenceEqual(result2));
+            List<uint> expectedResult2 = new List<uint> { 4, 0, 0, 0, 0 };
 
             List<uint> test3 = new List<uint> { 1 };
-            Debug.Assert(SortDescending(test3).SequenceEqual(test3));   // This one should be the same.
+            List<uint> expectedResult3 = test3;     // This one should be the same.
 
-            List<uint> test4 = new List<uint> { };
-            Debug.Assert(SortDescending(test4).SequenceEqual(test4));   // This one should be the same.
+            List<uint> test4 = new List<uint>();
+            List<uint> expectedResult4 = test4;     // This one should be the same.
+
+            // Act
+            List<uint> result1 = SortDescending(test1);
+            List<uint> result2 = SortDescending(test2);
+            List<uint> result3 = SortDescending(test3);
+            List<uint> result4 = SortDescending(test4);
+
+            // Assert
+            Assert.True(result1.SequenceEqual(expectedResult1));
+            Assert.True(result2.SequenceEqual(expectedResult2));
+            Assert.True(result3.SequenceEqual(expectedResult3));
+            Assert.True(result4.SequenceEqual(expectedResult4));
         }
 
         // Run provided test examples for Warmup 3 (LengthCheck).
-        private static void TestLengthCheck()
+        [Fact]
+        public static void TestLengthCheck()
         {
-            Debug.Assert(false == LengthCheck(7, new List<uint> { 6, 5, 5, 3, 2, 2, 2 }));
-            Debug.Assert(false == LengthCheck(5, new List<uint> { 5, 5, 5, 5, 5 }));
-            Debug.Assert(true == LengthCheck(5, new List<uint> { 5, 5, 5, 5 }));
-            Debug.Assert(true == LengthCheck(3, new List<uint> { 1, 1 }));
-            Debug.Assert(true == LengthCheck(1, new List<uint>()));
-            Debug.Assert(false == LengthCheck(0, new List<uint>()));
+            // Arrange
+            List<uint> test1 = new List<uint> { 6, 5, 5, 3, 2, 2, 2 };
+            List<uint> test2 = new List<uint> { 5, 5, 5, 5, 5 };
+            List<uint> test3 = new List<uint> { 5, 5, 5, 5 };
+            List<uint> test4 = new List<uint> { 1, 1 };
+            List<uint> test5 = new List<uint>();
+            List<uint> test6 = new List<uint>();
+
+            // Act
+            bool result1 = LengthCheck(7, test1);
+            bool result2 = LengthCheck(5, test2);
+            bool result3 = LengthCheck(5, test3);
+            bool result4 = LengthCheck(3, test4);
+            bool result5 = LengthCheck(1, test5);
+            bool result6 = LengthCheck(0, test6);
+
+            // Assert
+            Assert.False(result1);
+            Assert.False(result2);
+            Assert.True(result3);
+            Assert.True(result4);
+            Assert.True(result5);
+            Assert.False(result6);
         }
 
         // Run provided test examples for Warmup 4 (FrontElimination).
-        private static void TestFrontElimination()
+        [Fact]
+        public static void TestFrontElimination()
         {
+            // Arrange
             List<uint> test1 = new List<uint> { 5, 4, 3, 2, 1 };
-            List<uint> result1 = new List<uint> { 4, 3, 2, 1, 1 };
-            Debug.Assert(FrontElimination(4, test1).SequenceEqual(result1));
+            List<uint> expectedResult1 = new List<uint> { 4, 3, 2, 1, 1 };
 
             List<uint> test2 = new List<uint> { 14, 13, 13, 13, 12, 10, 8, 8, 7, 7, 6, 6, 4, 4, 2 };
-            List<uint> result2 = new List<uint> { 13, 12, 12, 12, 11, 9, 7, 7, 6, 6, 5, 6, 4, 4, 2 };
-            Debug.Assert(FrontElimination(11, test2).SequenceEqual(result2));
+            List<uint> expectedResult2 = new List<uint> { 13, 12, 12, 12, 11, 9, 7, 7, 6, 6, 5, 6, 4, 4, 2 };
 
             List<uint> test3 = new List<uint> { 10, 10, 10 };
-            List<uint> result3 = new List<uint> { 9, 10, 10};
-            Debug.Assert(FrontElimination(1, test3).SequenceEqual(result3));
+            List<uint> expectedResult3 = new List<uint> { 9, 10, 10 };
 
             List<uint> test4 = new List<uint> { 10, 10, 10 };
-            List<uint> result4 = new List<uint> { 9, 9, 9 };
-            Debug.Assert(FrontElimination(3, test4).SequenceEqual(result4));
+            List<uint> expectedResult4 = new List<uint> { 9, 9, 9 };
 
             List<uint> test5 = new List<uint> { 1 };
-            List<uint> result5 = new List<uint> { 0 };
-            Debug.Assert(FrontElimination(1, test5).SequenceEqual(result5));
+            List<uint> expectedResult5 = new List<uint> { 0 };
+
+            // Act
+            List<uint> result1 = FrontElimination(4, test1);
+            List<uint> result2 = FrontElimination(11, test2);
+            List<uint> result3 = FrontElimination(1, test3);
+            List<uint> result4 = FrontElimination(3, test4);
+            List<uint> result5 = FrontElimination(1, test5);
+
+            // Assert
+            Assert.True(result1.SequenceEqual(expectedResult1));
+            Assert.True(result2.SequenceEqual(expectedResult2));
+            Assert.True(result3.SequenceEqual(expectedResult3));
+            Assert.True(result4.SequenceEqual(expectedResult4));
+            Assert.True(result5.SequenceEqual(expectedResult5));
         }
 
         // Run provided test examples for actual exercise (HavelHakimi).
-        private static void TestHavelHakimi()
+        [Fact]
+        public static void TestHavelHakimi()
         {
+            // Arrange
             List<uint> test1 = new List<uint> { 5, 3, 0, 2, 6, 2, 0, 7, 2, 5 };
-            Debug.Assert(false == HavelHakimi(test1));
-
             List<uint> test2 = new List<uint> { 4, 2, 0, 1, 5, 0 };
-            Debug.Assert(false == HavelHakimi(test2));
-
             List<uint> test3 = new List<uint> { 3, 1, 2, 3, 1, 0 };
-            Debug.Assert(true == HavelHakimi(test3));
-
             List<uint> test4 = new List<uint> { 16, 9, 9, 15, 9, 7, 9, 11, 17, 11, 4, 9, 12, 14, 14, 12, 17, 0, 3, 16 };
-            Debug.Assert(true == HavelHakimi(test4));
-
             List<uint> test5 = new List<uint> { 14, 10, 17, 13, 4, 8, 6, 7, 13, 13, 17, 18, 8, 17, 2, 14, 6, 4, 7, 12 };
-            Debug.Assert(true == HavelHakimi(test5));
-
             List<uint> test6 = new List<uint> { 15, 18, 6, 13, 12, 4, 4, 14, 1, 6, 18, 2, 6, 16, 0, 9, 10, 7, 12, 3 };
-            Debug.Assert(false == HavelHakimi(test6));
-
             List<uint> test7 = new List<uint> { 6, 0, 10, 10, 10, 5, 8, 3, 0, 14, 16, 2, 13, 1, 2, 13, 6, 15, 5, 1 };
-            Debug.Assert(false == HavelHakimi(test7));
-
             List<uint> test8 = new List<uint> { 2, 2, 0 };
-            Debug.Assert(false == HavelHakimi(test8));
-
             List<uint> test9 = new List<uint> { 3, 2, 1 };
-            Debug.Assert(false == HavelHakimi(test9));
-
             List<uint> test10 = new List<uint> { 1, 1 };
-            Debug.Assert(true == HavelHakimi(test10));
-
             List<uint> test11 = new List<uint> { 1 };
-            Debug.Assert(false == HavelHakimi(test11));
-
             List<uint> test12 = new List<uint> { };
-            Debug.Assert(true == HavelHakimi(test12));
+
+            // Act
+            bool result1 = HavelHakimi(test1);
+            bool result2 = HavelHakimi(test2);
+            bool result3 = HavelHakimi(test3);
+            bool result4 = HavelHakimi(test4);
+            bool result5 = HavelHakimi(test5);
+            bool result6 = HavelHakimi(test6);
+            bool result7 = HavelHakimi(test7);
+            bool result8 = HavelHakimi(test8);
+            bool result9 = HavelHakimi(test9);
+            bool result10 = HavelHakimi(test10);
+            bool result11 = HavelHakimi(test11);
+            bool result12 = HavelHakimi(test12);
+
+            // Assert
+            Assert.False(result1);
+            Assert.False(result2);
+            Assert.True(result3);
+            Assert.True(result4);
+            Assert.True(result5);
+            Assert.False(result6);
+            Assert.False(result7);
+            Assert.False(result8);
+            Assert.False(result9);
+            Assert.True(result10);
+            Assert.False(result11);
+            Assert.True(result12);
         }
     }
 }
